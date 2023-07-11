b5ae84787de4aa4959926effdd2c9bdd28004a9a
Faiza Farooq <105562536+FaizaFarooq23@users.noreply.github.com>
2023-07-11 23:35:49 +0500

    hero section created

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Home from './page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className='w-screen h-screen'>
+     <Home/>
+     </div>
     </html>
   )
 }
