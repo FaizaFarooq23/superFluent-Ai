1c22f23c8db7fa945aafeab94307bc82dcc09793
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-07-14 22:15:03 +0500

    fixed ui merge

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Home from "./page";
-
+import Header from "./header";
+import Footer from "./footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
