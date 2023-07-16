c9b1fd60c88134de709f7d9f0c2dc433904108d7
Faiza Farooq <105562536+FaizaFarooq23@users.noreply.github.com>
2023-07-16 20:17:21 +0500

    speaking page created

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Footer from "./components/homePage/footer";
 import HeroSection from "./components/homePage/heroSection";
 import Navbar from "./components/homePage/navbar";
 import Pricing from "./components/homePage/pricing";
+
 import RootLayout from "./layout";
 
 export default function Home() {
